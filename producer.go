f1af8a3817a07043e18a0f711738346bdba2da18
Falcon-slayer123 <himanshutrivedi989@gmail.com>
2025-10-07 16:57:55 +0530

    added producer & created API using Fiber

---
+++ producer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message":err,
+		})
+         return err
+
+	}
+
+	
+
+}
